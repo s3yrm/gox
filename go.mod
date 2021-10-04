78f86764afb5a573696c9574244f159b1d4241b7
s3yrm <s3yrm@fedora.email>
2021-10-04 08:53:22 +0530

    Completely rewrote the application. Editor now opens within the terminal and allows user input.

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/s3yrm/gox
 
 go 1.17
 
-require github.com/rs/zerolog v1.25.0 // indirect
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/rs/zerolog v1.25.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rs/zerolog"
 )
 
@@ -22,9 +23,7 @@ func main() {
 	}
 	defer logfile.Close()
 
-	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
-	multi := zerolog.MultiLevelWriter(consoleWriter, logfile)
-	Logger = zerolog.New(multi).With().Timestamp().Logger()
+	Logger = zerolog.New(logfile).With().Timestamp().Logger()
 	Logger = Logger.With().Caller().Logger()
 
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
@@ -32,15 +31,92 @@ func main() {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 
-	// Remove this to pass filename from commandline //
-	file := File{
-		Name:   "Goxfile",
-		Logger: Logger,
-	}
-
-	err = file.Open()
+	// begin setting up the UI
+	scr, err := tcell.NewScreen()
 	if err != nil {
-		Logger.Error().Err(err).Msg("Error working with file.")
+		Logger.Fatal().Msg("Cannot Create screen")
 	}
 
+	err = scr.Init()
+	if err != nil {
+		Logger.Fatal().Msg("Cannot Initialize screen")
+	}
+
+	style := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	scr.SetStyle(style)
+	scr.DisableMouse()
+
+	scr.Clear()
+
+	scrWidth, scrHeight := scr.Size()
+
+	// Draw line begin indicators
+	for row, col := 0, scrHeight; col >= 0; col-- {
+		scr.SetContent(row, col, '~', nil, style)
+	}
+
+	// Place cursor at the top left and show screen
+	scr.ShowCursor(0, 0)
+
+	// row and col to keep track of cursor on screen; init at (0,0)
+	row, col := 0, 0
+
+	// quit the application
+	quit := func() {
+		scr.Fini()
+		os.Exit(0)
+	}
+
+	for {
+		// Show screen
+		scr.Show()
+
+		// Poll events
+		ev := scr.PollEvent()
+
+		// Process events
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			scrWidth, scrHeight = scr.Size()
+			scr.Sync()
+			for firstCol := scrHeight; firstCol >= 0; firstCol-- {
+				scr.SetContent(0, firstCol, '~', nil, style)
+			}
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			}
+			if ev.Key() == tcell.KeyRune {
+				// If the row rows more than the width of the screen, insert cursor and content in new line, beginning at 0.
+				if row >= scrWidth {
+					col++
+					row = 0
+					Logger.Debug().Msgf("Line break; row: %d, Col: %d", row, col)
+				}
+				scr.SetContent(row, col, ev.Rune(), nil, style)
+				scr.ShowCursor(row+1, col) // cursor will always be one rune ahead.
+				row++
+
+			}
+			if ev.Key() == tcell.KeyBackspace2 {
+				scr.SetContent(row-1, col, rune(tcell.KeyNUL), nil, style) // rune appearing before the cursor is reset.
+				row--
+
+				if row <= 0 && col <= 0 { // If the cursor is at origin, do not do anything.
+					Logger.Debug().Msg("Cursor at origin.")
+					row, col = 0, 0
+					scr.ShowCursor(0, 0)
+					break
+				} else if row <= 0 && col > 0 { // If the content is multiple lines long and cursor is at first row, place it 
+				Logger.Debug().Msgf("Line cleared, moving to previous line; row: %d, col: %d", row, col)
+					row = scrWidth // at the end of the previous line.
+					col--
+					scr.ShowCursor(row, col)
+					break
+				} else {
+					scr.ShowCursor(row, col)
+				}
+			}
+		}
+	}
 }
